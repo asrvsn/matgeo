74e76d59f3d4363b0bf793cc383a7b736358a509
Anand Srinivasan <anand@a0s.co>
2024-10-08 13:54:27 +0100

    upgrades

--- src/matgeo/__init__.py
+++ src/matgeo/__init__.py
@@ -1,3 +1,3 @@
-from .ellipsoid import Ellipsoid, Sphere
+from .ellipsoid import Ellipsoid, Sphere, Circle
 from .plane import Plane, PlanarPolygon
 from .triangulation import Triangulation
--- src/matgeo/ellipsoid.py
+++ src/matgeo/ellipsoid.py
@@ -226,6 +226,11 @@ class Ellipsoid:
 
     def __truediv__(self, x: float):
         return Ellipsoid(self.M * (x**2), self.v.copy())
+    
+    def transpose(self, *idxs: Tuple) -> 'Ellipsoid':
+        assert len(idxs) == self.ndim, 'Must have same dimension as ellipsoid'
+        idxs = list(idxs)
+        return Ellipsoid(self.M.copy()[idxs, :][:, idxs], self.v.copy()[idxs])
 
     def contains(self, x: np.ndarray) -> np.ndarray:
         ''' Whether the ellipsoid contains said points '''
@@ -399,7 +404,7 @@ class Ellipsoid:
         return Ellipsoid.n_sphere(1, v=v, r=r)
     
     @staticmethod
-    def sphere(v: np.ndarray=None) -> 'Ellipsoid':
+    def sphere(v: np.ndarray=None, r: float=1) -> 'Ellipsoid':
         return Ellipsoid.n_sphere(2, v=v, r=r)
 
 class Sphere(Ellipsoid):
@@ -420,11 +425,58 @@ class Sphere(Ellipsoid):
     def r(self, r: float):
         self.M = np.eye(self.ndim) * r**(-2)
 
+    def __add__(self, x: np.ndarray):
+        return Sphere(self.v + x, self.r)
+
+    def __sub__(self, x: np.ndarray):
+        return Sphere(self.v - x, self.r)
+
+    def __mul__(self, x: float):
+        return Sphere(self.v.copy(), self.r * x)
+
+    def __truediv__(self, x: float):
+        return Sphere(self.v.copy(), self.r / x)
+
     @staticmethod
     def from_poly(poly: PlanarPolygon) -> 'Sphere':
         v = poly.centroid()
         r = np.sqrt(poly.area() / np.pi)
         return Sphere(v, r)
+    
+    @staticmethod
+    def from_ellipsoid(ell: Ellipsoid) -> 'Sphere':
+        d = ell.ndim
+        M_ = np.eye(d) * np.trace(ell.M) / d
+        assert np.allclose(M_, ell.M), 'Ellipse matrix must be isotropic to cast into sphere'
+        r = ell.M[0,0] ** (-1/2)
+        return Sphere(ell.v, r)
+    
+class Circle(Sphere):
+    def __init__(self, v: np.ndarray, r: float):
+        assert v.shape == (2,), 'v must be 2d'
+        super().__init__(v, r)
+
+    def __add__(self, x: np.ndarray):
+        return Circle(self.v + x, self.r)
+
+    def __sub__(self, x: np.ndarray):
+        return Circle(self.v - x, self.r)
+
+    def __mul__(self, x: float):
+        return Circle(self.v.copy(), self.r * x)
+
+    def __truediv__(self, x: float):
+        return Circle(self.v.copy(), self.r / x)
+
+    def flipy(self, yval: float) -> 'Circle':
+        ''' Flip the y-coordinate of the center of the circle '''
+        return Circle(np.array([self.v[0], yval-self.v[1]]), self.r)
+    
+    @staticmethod
+    def from_ellipse(ell: Ellipsoid) -> 'Circle':
+        assert ell.ndim == 2, 'Ellipse must be 2d to cast into circle'
+        sph = Sphere.from_ellipsoid(ell)
+        return Circle(sph.v, sph.r)
 
 if __name__ == '__main__':
 
